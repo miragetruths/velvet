9896adc1a4b85c508d3c6581faf23ddd1b594306
Mirage <ic.miragetruths@gmail.com>
2026-06-25 14:42:31 +0530

    Update monitors.lua

--- .config/hypr/modules/monitors.lua
+++ .config/hypr/modules/monitors.lua
@@ -1,11 +1,16 @@
-------------------
----- MONITORS ----
-------------------
 
--- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+----> MONITORS
+
+
+-- If you want to tweak your display, check out the wiki.
+-- https://wiki.hypr.land/Configuring/Basics/Monitors/
+
 hl.monitor({
     output   = "eDP-1",
     mode     = "1920x1080@144.00301",
     position = "0x0",
     scale    = "1",
 })
+
+-- If yours values are different, run 'hyprctl monitors' and input the values you see.
+-- If you have multiple monitors, I am sorry and envious as I don't.
